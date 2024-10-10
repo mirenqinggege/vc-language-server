d091d927800a885cd2e350e2b1af5fa1fad9ea89
GitHub Action <action@github.com>
2024-10-10 03:38:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/mirenqinggege/vclanguageserver/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mirenqinggege/vclanguageserver/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mirenqinggege.vclanguageserver.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
